09e520bf404cd6b93c179d044775e5c37443ea35
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-08 15:38:53 +0530

    Create Home page
    - Create base template so that it can be extended
    - Create index template for index view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Blog, Blogger
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Working")
+    
+    num_blog = Blog.objects.all().count()
+
+    num_blogger = Blogger.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_blog': num_blog,
+        'num_blogger': num_blogger,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
