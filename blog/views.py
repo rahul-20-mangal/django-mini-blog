da051d2402b0fc208277b9ebcbd96e85e878fbeb
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-11 14:54:19 +0530

    Create blog and comment page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from blog.models import Blog, Blogger
 from django.views import generic
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from blog.forms import CommentForm
+from blog.forms import CommentForm, BlogForm
+from django.utils import timezone
+import datetime
+
 # Create your views here.
 
 def index(request):
@@ -46,8 +49,39 @@ class BloggerDetailView(generic.DetailView):
 
 @login_required
 def add_comment(request, pk):
-    form = CommentForm(request.POST)
-    context = {'form':form}
-    return render(request, 'blog/add_comment.html', context)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
 
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.blog = get_object_or_404(Blog, pk = pk)
+            comment.date_time = timezone.now()
+            comment.user = request.user
+            comment.save()
+            
+            return redirect('blog-detail', pk=pk)
     
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/add_comment.html', {'form': form})
+
+@login_required
+def create_blog(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+
+        if form.is_valid():
+            blog = form.save(commit=False)
+            if Blogger.objects.filter(name=request.user).exists():
+                blog.blog_author = Blogger.objects.get(name=request.user)
+            else:   
+                blog.blog_author = Blogger.objects.create(name=request.user)
+            blog.post_date = datetime.date.today()
+            blog.save()
+            return redirect('blogs')
+    
+    else:
+        form = BlogForm()
+
+    return render(request, 'blog/create_blog.html', {'form': form})
