be5237b4977c26896303e282acd459f0b8c1a061
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-11 14:55:52 +0530

    Update database

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('blogs/', views.BlogListView.as_view(), name='blogs'),
-    path('<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
+    path('blog/<int:pk>', views.BlogDetailView.as_view(), name='blog-detail'),
     path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
     path('blogger/<int:pk>', views.BloggerDetailView.as_view(), name='blogger-detail'),
-    path('blog/<int:pk>/addcomment/',views.add_comment, name='add-comment'),
+    path('addcomment/<int:pk>/',views.add_comment, name='add-comment'),
+    path('create-blog/', views.create_blog, name='create-blog'),
 ]
